6d9486233e111ef84662ed99cfe322956c013b39
terry <terry@szwesion.com>
2016-12-08 17:50:20 +0800

    initial root commit

---
+++ root.mk
@@ -0,0 +1,15 @@
+PRODUCT_PACKAGES += \
+				 SuperSU
+
+PRODUCT_COPY_FILES += \
+				vendor/amlogic/root/install-recovery.sh:system/etc/install-recovery.sh  \
+				vendor/amlogic/root/install-recovery.sh:system/bin/install-recovery.sh  \
+				vendor/amlogic/root/su:system/xbin/su \
+				vendor/amlogic/root/su:system/bin/.ext/.su \
+				vendor/amlogic/root/su:system/xbin/daemonsu \
+			    vendor/amlogic/root/su:system/xbin/sugote \
+			    vendor/amlogic/root/supolicy:system/xbin/supolicy \
+			    vendor/amlogic/root/libsupol.so:system/lib/libsupol.so \
+			    vendor/amlogic/root/sugote-mksh:system/xbin/sugote-mksh \
+                vendor/amlogic/root/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+			    vendor/amlogic/root/.installed_su_daemon:system/etc/.installed_su_daemon
